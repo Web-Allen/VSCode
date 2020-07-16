489cd47d6273521f842a2c7972a43018717b9252
Web-Allen <JingTian0805@gmail.com>
2020-07-16 22:57:54 +0800

    7/16

---
+++ My Record/Home/nav-title.js
@@ -0,0 +1,3 @@
+document.write( '<a href=\"#\" class=\"logo\"><i class=\"fas fa-book-open fa-lg\"></i></a>\n' );
+document.write( '            <h1 id=\"nav-title\">My Record</h1>\n' );
+document.write( '            <a href=\"\">首頁</a>' );
